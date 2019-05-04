8a1d4a7b6d7ebc3b6d1ef0f58e528c2e1cf4cf59
Rana Awais <rana.awais@ebryx.com>
2019-05-04 21:07:05 +0500

    downloads chrome package in runtime, if needed.

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import os
+import stat
 import time
 import json
+import zipfile
 import logging
 
 import requests
@@ -12,7 +14,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import (
+    StaleElementReferenceException, TimeoutException)
 
 
 logger = logging.getLogger(__name__)
@@ -67,24 +70,61 @@ def read_config():
 
 def validate_config(config):
 
-    for key in ['usm_host_url', 'usm_username', 'usm_password']:
+    for key in ['usm_host_url', 'usm_username', 'usm_password',
+                'chrome_package']:
         if not config.get(key):
             _exit(400, 'No `%s` field defined in config.' % (key))
 
 
-def alert_on_slack(data, config):
+def get_chrome_folder(config):
 
+    if config['chrome_package'].startswith(('http', 'https')):
+        package_folder = '/tmp/chrome_package'
+        os.mkdir(package_folder) if not os.path.isdir(
+            package_folder) else str()
+        package_path = os.path.join(package_folder, 'archive.zip')
+        if not os.path.isfile(package_path):
+            logger.info('Downloading chrome package...')
+            res = requests.get(config['chrome_package'])
+            if res.status_code >= 400:
+                _exit(res.status_code, 'Unexpected response while '
+                      'fetching chrome package')
+
+            open(package_path, 'wb').write(res.content)
+            logger.info('Writing package to: %s', package_path)
+        else:
+            logger.info('Skipping download. Archive already '
+                        'present: %s', package_path)
+
+        logger.info('Unzipping archive...')
+        zfile = zipfile.ZipFile(package_path, 'r')
+        zfile.extractall(package_folder)
+
+        logger.info('Making binaries executeable...')
+        for name in ['chromedriver', 'headless-chromium']:
+            filename = os.path.join(package_folder, name)
+            if os.path.isfile(filename):
+                st = os.stat(filename)
+                os.chmod(filename, st.st_mode | stat.S_IEXEC)
+
+        return package_folder
+    else:
+        return config['chrome_package']
+
+
+def get_slack_text(data, config):
+
+    prepared_string = str()
     logger.info(str())
     logger.info('[%d] sensors are found to be disconnected.', len(data))
     if not config.get('slack_hooks', list()):
         logger.info('No webhook is specified. Skipping slack push.')
-        return
+        return prepared_string
 
     if not data:
         logger.info('No disconnected sensor detected. Skipping slack push.')
-        return
-
-    prepared_string = str()
+        return prepared_string
+    
     for entry in data:
         if not(entry.get('name') or entry.get('ip')):
             continue
@@ -102,13 +142,19 @@ def alert_on_slack(data, config):
     else:
         logger.info('Disconnected sensor(s) were whitelisted. '
                     'Skipping slack push.')
+        return prepared_string
+
+
+def alert_on_slack(text, config):
+
+    if not text:
         return
 
     for url in config['slack_hooks']:
         response, _count = (None, 0)
         while not response and _count < 5:
             try:
-                response = requests.post(url, json={'text': prepared_string})
+                response = requests.post(url, json={'text': text})
             except:
                 logger.info('Could not send slack request. ' +
                             'Retrying after 10 secs...')
@@ -140,6 +186,20 @@ def do_login(driver, config):
     login_elem.click()
 
 
+def wait_for_element(driver, selector, timeout=30, count=1):
+
+    if count > 3:
+        return {'exit': True}
+
+    try:
+        WebDriverWait(driver, 30).until(
+            EC.visibility_of_element_located((
+                By.CSS_SELECTOR, selector)))
+    except TimeoutException:
+        logger.info('Browser timed out while waiting...')
+        wait_for_element(driver, selector, timeout=timeout, count=count+1)
+
+
 def get_disconnected_sensors(driver):
 
     disconnected = list()
@@ -168,10 +228,18 @@ def get_disconnected_sensors(driver):
 def main(event, context):
 
     config = read_config()
+    folder = get_chrome_folder(config)
+    logger.info('Using chrome folder: %s', folder)
+    logger.info(str())
 
     options = Options()
     options.headless = True
-    driver = webdriver.Chrome('drivers/chromedriver', chrome_options=options)
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    options.binary_location = os.path.join(folder, 'headless-chromium')
+    driver = webdriver.Chrome(os.path.join(
+        folder, 'chromedriver'), chrome_options=options)
 
     try:
         logger.info('Visiting url: %s', config['usm_host_url'])
@@ -184,18 +252,22 @@ def main(event, context):
         do_login(driver, config)
 
         logger.info('Waiting for dashboard...')
-        WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((
-                By.CSS_SELECTOR, 'av-header #header')))
+        res = wait_for_element(driver, 'av-header #header')
+        if res and res.get('exit'):
+            alert_on_slack('Browser timed out after 3rd retry '
+                           'of waiting for element.', config)
+            _exit(400, 'Exiting program after 3rd retry...')
 
         sensor_link = driver.find_element_by_css_selector('a#nav-link-sensors')
         logger.info('Visting sensors page link...')
         driver.get(sensor_link.get_attribute('href'))
 
         logger.info('Waiting for sensors page...')
-        WebDriverWait(driver, 15).until(
-            EC.visibility_of_element_located((
-                By.CSS_SELECTOR, '#table-sensors-list')))
+        res = wait_for_element(driver, '#table-sensors-list', timeout=15)
+        if res and res.get('exit'):
+            alert_on_slack('Browser timed out after 3rd retry '
+                           'of waiting for element.', config)
+            _exit(400, 'Exiting program after 3rd retry...')
 
         logger.info('Finding disconnected sensors from page...')
         disconn = get_disconnected_sensors(driver)
@@ -203,7 +275,8 @@ def main(event, context):
         logger.info('Closing webdriver...')
         driver.close()
 
-        alert_on_slack(disconn, config)
+        text = get_slack_text(disconn, config)
+        alert_on_slack(text, config)
 
     except Exception as exc:
         logger.info('Exception occured in code. Gracefully closing webdriver.')
